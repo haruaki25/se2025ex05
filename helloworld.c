5845299186744cd052824c8822d6be25e31d606a
haruaki <r202404731ce@jindai.jp>
2025-12-23 23:23:07 +0900

    while v

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(void) {
   int i = 20;
-  printf("hello world %d\n", i);
-  return 0;
+  while (i < 10) {
+    printf("hello world %d\n", i);
+    i++;
+    return 0;
+  }
 }
